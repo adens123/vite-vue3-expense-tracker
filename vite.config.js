6668da681d32c1eb5df638d352a260f55d03511c
Jacky <adens123@gmail.com>
2024-01-30 20:41:35 +0800

    修改config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  base:
+    process.env.NODE_ENV === "production" ? "/vite-vue3-expense-tracker/" : "/"
+});
